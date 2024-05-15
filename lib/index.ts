b48aaee765a0c47c65143b91611a62805ff8e2cc
pengbo43 <pengbo43@jd.com>
2024-05-15 21:50:31 +0800

    feat: support matchMedia

--- lib/index.ts
+++ lib/index.ts
@@ -1,62 +1,4 @@
 export type Bound = Record<'top' | 'right' | 'bottom' | 'left', number>
-/**
- *
- * @param el HTMLElement
- * @param bound number[] => [top, right, bottom, left], drag bound value
- */
-export default function bindMove(el: HTMLElement, boundInfo: Partial<Bound>) {
-  const bound: Bound = Object.assign(
-    {
-      top: 70,
-      right: 20,
-      bottom: 50,
-      left: 20,
-    },
-    boundInfo,
-  )
-  el.style.position = 'fixed'
-  let startPos = {} as {
-    diffLeft: number
-    diffRight: number
-    diffTop: number
-    diffBottom: number
-  }
-
-  el.addEventListener('touchstart', (e) => {
-    const { clientY, clientX } = e.touches[0]
-    const curBound = el.getBoundingClientRect()
-
-    startPos = {
-      diffLeft: clientX - curBound.left,
-      diffRight: clientX - curBound.right,
-      diffTop: clientY - curBound.top,
-      diffBottom: clientY - curBound.bottom,
-    }
-  })
-
-  // 为 el 添加事件
-  el.addEventListener('touchmove', (e) => {
-    e.stopPropagation()
-    e.preventDefault()
-    const { clientHeight, clientWidth } = document.documentElement
-    const { clientHeight: h } = el
-    const { top, right, bottom, left } = bound
-    const { clientY, clientX } = e.touches[0]
-
-    const { diffTop, diffLeft } = startPos
-
-    let nextPos = clientY - diffTop
-    nextPos =
-      nextPos > clientHeight - bottom - h ? clientHeight - bottom - h : nextPos
-    nextPos = nextPos < top ? top : nextPos
-    el.style.top = `${nextPos}px`
-
-    nextPos = clientX - diffLeft
-    nextPos = nextPos > clientWidth - left ? clientWidth - left : nextPos
-    nextPos = nextPos < right ? right : nextPos
-    el.style.left = `${nextPos}px`
-  })
-}
 
 export function bindDrag(el: HTMLElement, boundInfo: Partial<Bound>) {
   const bound = Object.assign(
@@ -115,4 +57,71 @@ export function bindDrag(el: HTMLElement, boundInfo: Partial<Bound>) {
       startY: nextY,
     }
   })
+}
+
+export function bindTouch(el: HTMLElement, boundInfo: Partial<Bound>) {
+  const bound: Bound = Object.assign(
+    {
+      top: 70,
+      right: 20,
+      bottom: 50,
+      left: 20,
+    },
+    boundInfo,
+  )
+  el.style.position = 'fixed'
+  let startPos = {} as {
+    diffLeft: number
+    diffRight: number
+    diffTop: number
+    diffBottom: number
+  }
+
+  el.addEventListener('touchstart', (e) => {
+    const { clientY, clientX } = e.touches[0]
+    const curBound = el.getBoundingClientRect()
+
+    startPos = {
+      diffLeft: clientX - curBound.left,
+      diffRight: clientX - curBound.right,
+      diffTop: clientY - curBound.top,
+      diffBottom: clientY - curBound.bottom,
+    }
+  })
+
+  // 为 el 添加事件
+  el.addEventListener('touchmove', (e) => {
+    e.stopPropagation()
+    e.preventDefault()
+    const { clientHeight, clientWidth } = document.documentElement
+    const { clientHeight: h } = el
+    const { top, right, bottom, left } = bound
+    const { clientY, clientX } = e.touches[0]
+
+    const { diffTop, diffLeft } = startPos
+
+    let nextPos = clientY - diffTop
+    nextPos =
+      nextPos > clientHeight - bottom - h ? clientHeight - bottom - h : nextPos
+    nextPos = nextPos < top ? top : nextPos
+    el.style.top = `${nextPos}px`
+
+    nextPos = clientX - diffLeft
+    nextPos = nextPos > clientWidth - left ? clientWidth - left : nextPos
+    nextPos = nextPos < right ? right : nextPos
+    el.style.left = `${nextPos}px`
+  })
+}
+
+/**
+ *
+ * @param el HTMLElement
+ * @param bound number[] => [top, right, bottom, left], drag bound value
+ */
+export default function bindMove(el: HTMLElement, boundInfo: Partial<Bound>) {
+  // 使用 CSS 媒体查询和 window.matchMedia 来检测设备是否支持触摸：
+  if (window.matchMedia('(pointer: coarse)').matches) {
+    return bindMove(el, boundInfo)
+  }
+  return bindDrag(el, boundInfo)
 }
